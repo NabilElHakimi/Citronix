a1c235ffb33371159e07543d520194badd76d784
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-11-16 23:04:08 +0100

    refactor code

--- src/main/java/me/elhakimi/citronix/rest/vm/mapper/FarmMapper.java
+++ src/main/java/me/elhakimi/citronix/rest/vm/mapper/FarmMapper.java
@@ -3,12 +3,15 @@ package me.elhakimi.citronix.rest.vm.mapper;
 import me.elhakimi.citronix.domain.Farm;
 import me.elhakimi.citronix.rest.vm.FarmVm;
 import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
 
 @Mapper(componentModel = "spring")
 public interface FarmMapper {
 
+    @Mapping(source = "id", target = "id")
     FarmVm toFarmVm(Farm farm);
 
+    @Mapping(source = "id", target = "id")
     Farm toFarm(FarmVm farmVm);
 
 }
