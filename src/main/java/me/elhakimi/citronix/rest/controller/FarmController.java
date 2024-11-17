e316b58e3b7d5f1680b10e4cc7374f953e418dad
EL-Nabil-Hakimi <hakimi.deve@gmail.com>
2024-11-17 14:48:47 +0100

    fix the conflict

---
+++ src/main/java/me/elhakimi/citronix/rest/controller/FarmController.java
@@ -0,0 +1,12 @@
+package me.elhakimi.citronix.rest.controller;
+
+
+import lombok.AllArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/farm")
+@AllArgsConstructor
+public class FarmController {
+}
